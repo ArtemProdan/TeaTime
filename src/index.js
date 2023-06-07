99679a0c0f8340cbd16144dc75d6205f5b7c7bc6
ArtemProdan <131655201+ArtemProdan@users.noreply.github.com>
2023-06-07 13:55:18 +0300

    Hash Router dobavlen

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from './redux/redux-store';
 // import StoreContext from './StoreContext2';
 import { Provider } from 'react-redux';
@@ -12,7 +12,7 @@ const root = ReactDOMClient.createRoot(document.getElementById('root'));
 // let rerenderEntireTree = () => {
 
     root.render(
-            <BrowserRouter>
+            <HashRouter>
                 <Provider store={store}>
                     <App
                         store={store}
@@ -20,7 +20,7 @@ const root = ReactDOMClient.createRoot(document.getElementById('root'));
                     // dispatch={store.dispatch.bind(store)}
                     />
                 </Provider>
-            </BrowserRouter>
+            </HashRouter>
     )
 // }
 // rerenderEntireTree(store.getState())
